d8278f3b1351b4a3283ad093ddb6cbe21ebb29b6
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-02-18 10:20:11 +0000

    Basic web scaper function with BeautifulSoup and requests

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+search = input("Enter Search term: ")
+params = {"q": search}
+r = requests.get("https://www.bing.com/search", params=params)
+
+soup = BeautifulSoup(r.text, "html.parser")
+print(soup.prettify())
+
+
